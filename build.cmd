b88e74f89b851ca6d1be06bfc31a2263d45acab9
W192547975 <138874985+W192547975@users.noreply.github.com>
2023-12-09 19:36:51 +0800

    Edit build.cmd

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 @set GOPROXY
 if not defined GOPROXY set GOPROXY=https://goproxy.io,direct
 go mod tidy
-if ERRORLEVEL 1 pause ERRORLEVEL:%ERRORLEVEL%
+if ERRORLEVEL 1 pause (value:%ERRORLEVEL%)
 go build cmd/openp2p.go
 @echo.
 @pause
