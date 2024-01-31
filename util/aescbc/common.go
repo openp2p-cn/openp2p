91012a197c328929d519ef97f78020b5058dcb20
W192547975 <W192547975@163.com>
2024-01-31 22:00:37 +0800

    Split aescbc

---
+++ util/aescbc/common.go
@@ -0,0 +1,77 @@
+package util
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+)
+
+var cbcIVBlock = []byte("UHNJUSBACIJFYSQN")
+
+var paddingArray = [][]byte{
+	{0},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+	{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
+	{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
+	{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7},
+	{8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8},
+	{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+	{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
+	{11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11},
+	{12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12},
+	{13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13},
+	{14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
+	{15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15},
+	{16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16},
+}
+
+func pkcs7Padding(plainData []byte, dataLen, blockSize int) int {
+	padLen := blockSize - dataLen%blockSize
+	pPadding := plainData[dataLen : dataLen+padLen]
+
+	copy(pPadding, paddingArray[padLen][:padLen])
+	return padLen
+}
+
+func pkcs7UnPadding(origData []byte, dataLen int) ([]byte, error) {
+	unPadLen := int(origData[dataLen-1])
+	if unPadLen <= 0 || unPadLen > 16 {
+		return nil, fmt.Errorf("wrong pkcs7 padding head size:%d", unPadLen)
+	}
+	return origData[:(dataLen - unPadLen)], nil
+}
+
+// AES-CBC
+func encryptBytes(key []byte, out, in []byte, plainLen int) ([]byte, error) {
+	if len(key) == 0 {
+		return in[:plainLen], nil
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	// iv := out[:aes.BlockSize]
+	// if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+	// 	return nil, err
+	// }
+	mode := cipher.NewCBCEncrypter(block, cbcIVBlock)
+	total := pkcs7Padding(in, plainLen, aes.BlockSize) + plainLen
+	mode.CryptBlocks(out[:total], in[:total])
+	return out[:total], nil
+}
+
+func decryptBytes(key []byte, out, in []byte, dataLen int) ([]byte, error) {
+	if len(key) == 0 {
+		return in[:dataLen], nil
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	mode := cipher.NewCBCDecrypter(block, cbcIVBlock)
+	mode.CryptBlocks(out[:dataLen], in[:dataLen])
+	return pkcs7UnPadding(out, dataLen)
+}
---
+++ util/aescbc/common_test.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"log"
+	"testing"
+)
+
+func TestAESCBC(t *testing.T) {
+	for packetSize := 1; packetSize <= 8192; packetSize++ {
+		log.Println("test packetSize=", packetSize)
+		data := make([]byte, packetSize)
+		for i := 0; i < packetSize; i++ {
+			data[i] = byte('0' + i%10)
+		}
+		encryptBuf := make([]byte, len(data)+PaddingSize)
+		inBuf := make([]byte, len(data)+PaddingSize)
+		copy(inBuf, data)
+		cryptKey := []byte("0123456789ABCDEF")
+		sendBuf, err := encryptBytes(cryptKey, encryptBuf, inBuf, len(data))
+		if err != nil {
+			t.Errorf("encrypt packet failed: %v", err)
+		}
+		log.Printf("encrypt data len=%d\n", len(sendBuf))
+
+		decryptBuf := make([]byte, len(sendBuf))
+		outBuf, err := decryptBytes(cryptKey, decryptBuf, sendBuf, len(sendBuf))
+		if err != nil {
+			t.Errorf("decrypt packet failed:%s", err)
+		}
+		// log.Printf("len=%d,content=%s\n", len(outBuf), outBuf)
+		log.Printf("decrypt data len=%d\n", len(outBuf))
+		log.Println("validate")
+		for i := 0; i < len(outBuf); i++ {
+			if outBuf[i] != byte('0'+i%10) {
+				t.Error("validate failed")
+			}
+		}
+		log.Println("validate ok")
+	}
+
+}
